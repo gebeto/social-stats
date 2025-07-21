0bf549092b1b9c37cd6f7adefb8f201ee8c0deab
gebeto <slavik.nychkalo@gmail.com>
2025-07-21 10:36:31 +0300

    add reddit

--- api/stats.ts
+++ api/stats.ts
@@ -2,21 +2,25 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { TikTokParser } from "../parsers/tiktok";
 import { YoutubeParser } from "../parsers/youtube";
 import { InstagramParser } from "../parsers/instagram";
+import { RedditParser } from "../parsers/reddit";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { name = "World" } = req.query;
   const tiktokParser = new TikTokParser();
   const youtubeParser = new YoutubeParser();
   const instagramParser = new InstagramParser();
-  const [tiktok, youtube, instagram] = await Promise.all([
+  const redditParser = new RedditParser();
+  const [tiktok, youtube, instagram, reddit] = await Promise.all([
     await tiktokParser.parse("slavik.nychkalo"),
     await youtubeParser.parse("slavik.nychkalo"),
     await instagramParser.parse("slavik.nychkalo"),
+    await redditParser.parse("gebet0"),
   ]);
   return res.json({
     message: `Hello ${name}!`,
     tiktok: tiktok,
     youtube: youtube,
     instagram: instagram,
+    reddit: reddit,
   });
 }
